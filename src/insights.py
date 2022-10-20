93916792d2ea3530490f380fd653d3f36725b172
erik-efl <erik.lima.f@outlook.com>
2022-10-20 19:08:21 -0300

    feat: :sparkles: Adding function filter_by_job_type

--- src/insights.py
+++ src/insights.py
@@ -11,22 +11,12 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
+    filter_jobs = []
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
-
+    for job in jobs:
+        if job["job_type"] == job_type:
+            filter_jobs.append(job)
+    return filter_jobs
 
 def get_unique_industries(path):
     """Checks all different industries and returns a list of them
