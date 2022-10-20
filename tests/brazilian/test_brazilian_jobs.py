40154ba5e86da245e6c5ee0b2f19da59fa0676b1
erik-efl <erik.lima.f@outlook.com>
2022-10-20 19:22:43 -0300

    feat: :sparkles: Adding function Reading

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,12 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    jobs = read_brazilian_file('tests/mocks/brazilian_jobs.csv')
+    for job in jobs:
+        assert job.get('titulo') is not None
+        assert job.get('salario') is not None
+        assert job.get('tipo') is not None
+        assert job.get('title') is not None
+        assert job.get('salary') is not None
+        assert job.get('type') is not None
