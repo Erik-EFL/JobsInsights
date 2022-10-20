392a02fd575bc349f50ca18bddd4e8b232727eba
erik-efl <erik.lima.f@outlook.com>
2022-10-20 19:50:35 -0300

    fix: :bug: Correct method called in the function

--- src/jobs.py
+++ src/jobs.py
@@ -4,7 +4,7 @@ import csv
 
 @lru_cache
 def read(path):
-    with open(path) as file:
-        read_file = csv.reader(file)
+    with open(path, mode='r') as file:
+        read_file = csv.DictReader(file)
         read_list = list(read_file)
     return read_list
