c5ec7db9f342846924daec1b0adb6a5125e282f6
erik-efl <erik.lima.f@outlook.com>
2022-10-20 19:16:53 -0300

    feat: :sparkles: Adding function filter_by_salary_range

--- src/insights.py
+++ src/insights.py
@@ -85,18 +85,12 @@ def matches_salary_range(job, salary):
 
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
+    filter_jobs = []
 
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    return []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                filter_jobs.append(job)
+        except ValueError:
+            print("ERROR: incorrect value type or data checked")
+    return filter_jobs
