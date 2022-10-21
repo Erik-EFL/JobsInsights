13c97b6c88af8698b52fe480e46bc0561f62df16
erik-efl <erik.lima.f@outlook.com>
2022-10-21 12:57:04 -0300

    test: :test_tube: Adding new test
    Create testes for sorting  criteria

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,18 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+from src.jobs import read
+from src.insights import get_max_salary, get_min_salary
 
 
 def test_sort_by_criteria():
-    pass
+    path = "src/jobs.csv"
+    get_all_jobs = read(path)
+
+    sort_by(get_all_jobs, "min_salary")
+    excepted = get_min_salary(path)
+    result = get_all_jobs[0]["min_salary"]
+    assert result == str(excepted)
+
+    sort_by(get_all_jobs, "max_salary")
+    excepted = get_max_salary(path)
+    result = get_all_jobs[0]["max_salary"]
+    assert result == str(excepted)
