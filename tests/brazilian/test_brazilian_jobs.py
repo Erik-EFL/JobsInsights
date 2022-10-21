55cf9ba3ee967abc9b9ea1405ca16546a6237467
erik-efl <erik.lima.f@outlook.com>
2022-10-20 23:38:44 -0300

    test: :test_tube: Adding new test
    creating a test for seach brazilian jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -2,11 +2,11 @@ from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    jobs = read_brazilian_file('tests/mocks/brazilian_jobs.csv')
+    jobs = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
     for job in jobs:
-        assert job.get('titulo') is not None
-        assert job.get('salario') is not None
-        assert job.get('tipo') is not None
-        assert job.get('title') is not None
-        assert job.get('salary') is not None
-        assert job.get('type') is not None
+        assert job.get("titulo") is None
+        assert job.get("salario") is None
+        assert job.get("tipo") is None
+        assert job.get("title") is not None
+        assert job.get("salary") is not None
+        assert job.get("type") is not None
