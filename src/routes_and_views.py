7dd3364974ae5b36703a68a09822cb560976390b
erik-efl <erik.lima.f@outlook.com>
2022-10-21 13:24:37 -0300

    feat: :sparkles: Adding a new rout
    Create route job by id

--- src/routes_and_views.py
+++ src/routes_and_views.py
@@ -1,5 +1,6 @@
 from flask import Flask, Blueprint, render_template, request, send_file
 
+
 from .jobs import read
 from .insights import (
     get_unique_industries,
@@ -15,6 +16,7 @@ from .more_insights import (
     slice_jobs,
     get_int_from_args,
     build_jobs_urls,
+    get_job
 )
 
 bp = Blueprint("client", __name__, template_folder="templates")
@@ -72,6 +74,13 @@ def list_jobs():
     }
 
     return render_template("list_jobs.jinja2", ctx=ctx)
+
+
+@bp.route("/job/<index>")
+def job(index):
+    jobs = read(path="src/jobs.csv")
+    job = get_job(jobs, index)
+    return render_template("job.jinja2", job=job)
 
 
 def init_app(app: Flask):
