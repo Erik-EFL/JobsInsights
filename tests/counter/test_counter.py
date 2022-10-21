a41eeb9f44c6874b889f95cc941f1f71950682c5
erik-efl <erik.lima.f@outlook.com>
2022-10-20 23:38:00 -0300

    test: :test_tube: Adding new test
    Create test for count recorrence words

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,11 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = "src/jobs.csv"
+    word = ["python", "PYTHON", "javascript", "JAVAscript"]
+    expected_results = [1639, 1639, 122, 122]
+
+    for index in range(len(word)):
+        count = count_ocurrences(path, word[index])
+        assert count == expected_results[index]
